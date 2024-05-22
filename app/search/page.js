e9d229e78d21a8071a78d7573ca636da48ced892
hana_milad <89672587+hanamilad@users.noreply.github.com>
2024-05-22 03:07:18 +0300

    comit

--- app/search/page.js
+++ app/search/page.js
@@ -1,31 +1,31 @@
-"use client"
-import Result from './Result'
-import React, { useEffect, useRef, useState } from 'react'
-import data from '../_components/json.json'
+// "use client"
+// import Result from './Result'
+// import React, { useEffect, useRef, useState } from 'react'
+// import data from '../_components/json.json'
 
-function Searchpage() {
-  const [value,setvalue]=useState()
-  const [Data,setData]=useState([])
-  const valueSearch = useRef()
-  const handelchange = () => {
-    setvalue(valueSearch.current.value)
-  }
+// function Searchpage() {
+//   const [value,setvalue]=useState()
+//   const [Data,setData]=useState([])
+//   const valueSearch = useRef()
+//   const handelchange = () => {
+//     setvalue(valueSearch.current.value)
+//   }
  
-  useEffect(()=>{ 
-    const filterdata = data.filter((ele) =>value && ele.name.includes(value.trim()));
-    setData(filterdata)
-    },[value])
+//   useEffect(()=>{ 
+//     const filterdata = data.filter((ele) =>value && ele.name.includes(value.trim()));
+//     setData(filterdata)
+//     },[value])
 
-  return (
-<>
+//   return (
+// <>
 
 
-    <div className='p-2 m-auto w-3/4 max-[600px]:w-[100%]  flex gap-1 items-center '>
-        <input ref={valueSearch} onChange={handelchange}     type='search' placeholder='search' autoFocus  className='border-none mt-2 w-full bg-gray-200 p-3  ' />
-    </div>
-    <Result value={value} Data={Data} />
-</>
-  )
-}
+//     <div className='p-2 m-auto w-3/4 max-[600px]:w-[100%]  flex gap-1 items-center '>
+//         <input ref={valueSearch} onChange={handelchange}     type='search' placeholder='search' autoFocus  className='border-none mt-2 w-full bg-gray-200 p-3  ' />
+//     </div>
+//     <Result value={value} Data={Data} />
+// </>
+//   )
+// }
 
-export default Searchpage
+// export default Searchpage
